fc24d6ec254168887af73be3891089a094f6c685
Hassan Alhaji <halhaji@samf.gov.sa>
2026-05-17 11:34:00 +0300

    Fix media paths, sticky footer layout, and add logout button

--- config/settings.py
+++ config/settings.py
@@ -114,12 +114,12 @@ LOCALE_PATHS = [
 ]
 
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
