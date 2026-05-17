0d8242281dd236da5f2f21db4f8dbd6d8aaba8af
Hassan Alhaji <halhaji@samf.gov.sa>
2026-05-17 11:37:19 +0300

    Add SECURE_PROXY_SSL_HEADER for Load Balancers

--- config/settings.py
+++ config/settings.py
@@ -155,6 +155,7 @@ CORS_ALLOWED_ORIGINS = config(
 
 # Security Settings for Production
 if not DEBUG:
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
     SECURE_SSL_REDIRECT = True
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
