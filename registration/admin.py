3b343831230bd2708b58e74eda74ba57c9b72c1c
Hassan Alhaji <halhaji@samf.gov.sa>
2026-05-17 10:33:13 +0300

    Register models in Django Admin

--- registration/admin.py
+++ registration/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Registration, SiteSettings
 
-# Register your models here.
+@admin.register(Registration)
+class RegistrationAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'national_id', 'mobile_number', 'application_date')
+    search_fields = ('first_name', 'last_name', 'national_id', 'mobile_number', 'email')
+    list_filter = ('application_date', 'has_driving_license', 'has_sports_license', 'has_previous_experience')
+
+@admin.register(SiteSettings)
+class SiteSettingsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'is_registration_open')
